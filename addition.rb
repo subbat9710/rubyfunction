ad2c9d54c42fa7c9744dc164763fc0a3ac3672b0
Tula Ram Subba <tularamsubba@Tulas-MacBook-Air.local>
2017-01-25 12:09:23 -0500

    rubyfunction1

---
+++ addition.rb
@@ -0,0 +1,18 @@
+require "minitest/autorun"
+require_relative "testaddition.rb"
+
+class TestAddition < Minitest::Test
+
+def test_1_plus_1_returns_2
+	assert_equal(2,add(1,1))
+ end
+def test_2_plus_3_returns_5
+	assert_equal(5,add(2,3))
+ end
+def test_6_plus_4_returns_10
+	assert_equal(10,add(6,4))
+ end
+def test_8_plus_5_returns_13
+	assert_equal(13,add(8,5))
+ end
+end
